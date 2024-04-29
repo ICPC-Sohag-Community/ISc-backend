5ff47741577c87f7ad9f5e38680aaac14c34f4cb
Hossam Samy <hossamsamy234@gmail.com>
2024-04-30 02:43:20 +0300

    edit

--- ISc.Api/Program.cs
+++ ISc.Api/Program.cs
@@ -13,6 +13,7 @@ builder.Services
     .AddInfrastructure(builder.Configuration)
     .AddPresistance(builder.Configuration);
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- ISc.Infrastructure/Extension/ServiceCollection.cs
+++ ISc.Infrastructure/Extension/ServiceCollection.cs
@@ -7,6 +7,7 @@ using ISc.Application.Interfaces;
 using ISc.Infrastructure.Services.Email;
 using ISc.Infrastructure.Services.OnlineJudge.CodeForce;
 using ISc.Infrastructure.Services.ApiRequest;
+using ISc.Infrastructure.Services.Authentication;
 
 namespace ISc.Infrastructure.Extension
 {
@@ -17,7 +18,8 @@ namespace ISc.Infrastructure.Extension
             services.AddCollections()
                     .AddHangFireServices(configuration)
                     .AddFluentEmailServices(configuration)
-                    .AddMemoryCache();
+                    .AddMemoryCache()
+                    .AddDistributedMemoryCache();
                    
 
             return services;
@@ -56,7 +58,8 @@ namespace ISc.Infrastructure.Extension
                     .AddTransient<IEmailSender,EmailSender>()
                     .AddTransient<IEmailServices,EmailService>()
                     .AddTransient<IOnlineJudgeServices,CodeForceService>()
-                    .AddTransient<IApiRequestsServices,ApiReqeustService>();
+                    .AddTransient<IApiRequestsServices,ApiReqeustService>()
+                    .AddTransient<IAuthServices,AuthServices>();
 
             return services;
         }
