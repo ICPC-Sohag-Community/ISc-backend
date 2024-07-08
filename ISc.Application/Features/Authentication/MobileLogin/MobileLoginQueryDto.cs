8520adaf110410f8b57019195d05332ff681ef09
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-07-08 20:40:09 +0300

    implement mobile login

---
+++ ISc.Application/Features/Authentication/MobileLogin/MobileLoginQueryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISc.Application.Features.Authentication.MobileLogin
+{
+    public class MobileLoginQueryDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public IList<string> Roles { get; set; }
+        public string Token { get; set; }
+        public string? PhotoUrl { get; set; }
+    }
+}
