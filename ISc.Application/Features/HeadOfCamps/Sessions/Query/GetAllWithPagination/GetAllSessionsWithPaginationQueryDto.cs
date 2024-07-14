1c51794ab59e43658147857ebef8834972a55430
ahemmahersebap <ahemmahersebap@gmail.com>
2024-07-14 12:29:46 +0300

    Get Sessions by pagination

--- ISc.Application/Features/HeadOfCamps/Sessions/Query/GetAllWithPagination/GetAllSessionsWithPaginationQueryDto.cs
+++ ISc.Application/Features/HeadOfCamps/Sessions/Query/GetAllWithPagination/GetAllSessionsWithPaginationQueryDto.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ISc.Application.Features.HeadOfCamps.Sessions.Query.GetAllWithPagination
 {
-	internal class GetAllSessionsWithPaginationQueryDto
+	public class GetAllSessionsWithPaginationQueryDto
 	{
+		public int Id { get; set; }
+		public string Topic { get; set; }
+		public string InstructorName { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public string LocationName { get; set; }
 	}
 }
